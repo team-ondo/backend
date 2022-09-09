ea37d4690f2d968b68a18227d6a9e73bf80f087f
jonlld <87318928+jonlld@users.noreply.github.com>
2022-09-09 03:07:10 +0000

    feat: Add Device class to models and split files

--- src/models/user.py
+++ src/models/user.py
@@ -1,16 +1,17 @@
 from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy.orm import relationship
+# from sqlalchemy.orm import relationship
 
 from src.db.db import Base
 
 
 class User(Base):
+    # classes will inherit from Base
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    email = Column(String, unique=True, index=True)
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    email = Column(String(100), unique=True, index=True)
     phone_number = Column(String)
     created_at = Column(DateTime)
     updated_at = Column(DateTime)
