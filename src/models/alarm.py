52357b43b14a29774e7711e547f833f5465a4fde
jonlld <87318928+jonlld@users.noreply.github.com>
2022-09-09 03:42:11 +0000

    feat: Add Button and Alarm classes, update humidity to integer

--- src/models/alarm.py
+++ src/models/alarm.py
@@ -1,15 +1,15 @@
-from sqlalchemy import Column, Integer, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, Boolean, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 
 from src.db.db import Base
 
 
-class Humidity(Base):
-    __tablename__ = "humidity"
+class Alarm(Base):
+    __tablename__ = "alarm"
 
     id = Column(Integer, primary_key=True, index=True)
-    humidity = Column(Integer)
+    is_alarm = Column(Boolean)
     created_at = Column(DateTime)
     device_id = Column(Integer, ForeignKey("devices.id"))
 
-    owner = relationship("Device", back_populates="humidity_data")
+    owner = relationship("Device", back_populates="alarm_data")
