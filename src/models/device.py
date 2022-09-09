9556284371a2cfe7bec956d905cab5fed6c683e8
jonlld <87318928+jonlld@users.noreply.github.com>
2022-09-09 03:07:10 +0000

    feat: Add Device class to models and split files

---
+++ src/models/device.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, Float, String, DateTime, ForeignKey
+# from sqlalchemy.orm import relationship
+
+from src.db.db import Base
+
+
+class Device(Base):
+    __tablename__ = "devices"
+
+    id = Column(Integer, primary_key=True, index=True)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    device_name = Column(String(100))
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+    user_id = Column(Integer, ForeignKey("users.id"))
