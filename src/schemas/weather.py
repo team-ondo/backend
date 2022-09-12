fa1d6789176bf55b09e37e8f513ff68b36d02fc7
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-09-12 15:25:36 +0000

    feat: Add weather schema

---
+++ src/schemas/weather.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, Field
+
+
+class Weather(BaseModel):
+    location_name: str | None = Field(None, example="Horinouchi", description="Location name")
+    temperature_c: float | None = Field(None, example="23.72", description="Temperature (Celsius)")
+    temperature_f: float | None = Field(None, example="74.69", description="Temperature (Fahrenheit)")
+    humidity: int | None = Field(None, example="88", description="Humidity (%)")
+    weather_icon: str | None = Field(None, example="50n", description="Weather icon id")
+
+    class Config:
+        orm_mode = True
