6de40b0eaeea13a89d547bc6a6531cac53533f96
jonlld <87318928+jonlld@users.noreply.github.com>
2022-09-12 04:52:53 +0000

    chore: Update env file for base metadata

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -1,3 +1,4 @@
+from models.user import Base
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -18,7 +19,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
