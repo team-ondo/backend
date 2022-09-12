209167426ca55639d903d00f10f60ed874d3f477
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-09-12 15:28:35 +0000

    feat: Include weather router

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from src.routers import device
+from src.routers import weather
 
 app = FastAPI()
 app.include_router(device.router)
+app.include_router(weather.router)
 
 
 @app.get("/")
