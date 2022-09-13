246dc05c0c8386d35f4248566fe265a7b67a9739
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-09-13 06:57:11 +0000

    feat: Get latitude and longitude from devices table

--- src/routers/weather.py
+++ src/routers/weather.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 from requests.exceptions import RequestException
 from src.utils.common import convert_celsius_to_fahrenheit
 import src.schemas.weather as weather_schema
+import src.cruds.device as device_crud
+
 
 load_dotenv()
 router = APIRouter()
@@ -17,16 +19,12 @@ OPEN_WEATHER_APPID = os.getenv("OPEN_WEATHER_APPID")
 
 @ router.get("/weather-info/{lang}/{device_id}", response_model=weather_schema.Weather)
 async def read_weather_info(device_id: int, lang: str = "en", db: AsyncSession = Depends(get_db)):
-    # TODO Need to authenticate before fetching the device latitude and longitude
-
     if not (lang == "en" or lang == "ja"):
         raise HTTPException(status_code=404, detail="Only supports `en` or `ja`")
 
-    # TODO
-    # Get latitude and longitude from the device table
-    # result = await device_crud.get_latitude_and_longitude(db, device_id)
-    lat = 35.6762
-    lon = 139.6503
+    # TODO Need to authenticate before fetching the device latitude and longitude
+
+    lat, lon = await device_crud.get_latitude_and_longitude(db, device_id)
     params = {
         "lat": lat,
         "lon": lon,
