7c5db4b998408cf469c66aa1586008595edce214
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-09-13 14:44:11 +0000

    feat: Add DeviceDataCreate Schema

--- src/schemas/device.py
+++ src/schemas/device.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field
+from datetime import datetime
 
 
 class Device(BaseModel):
@@ -9,3 +10,13 @@ class Device(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class DeviceDataCreate(BaseModel):
+    temperature_c: float = Field(example="25.1", description="Temperature (Celsius)")
+    temperature_f: float = Field(example="77.18", description="Temperature (Fahrenheit)")
+    humidity: float = Field(example="87", description="Humidity")
+    motion: bool = Field(example=True, description="Motion detected")
+    alarm: bool = Field(example=True, description="Alarmed")
+    button: bool = Field(example=True, description="Button pressed")
+    created_at: datetime = Field(example="2022-07-01 12:23:45", description="Created timestamp")
