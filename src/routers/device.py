d71e90097b3a233efaae92cf46636df26fa5280b
jonlld <87318928+jonlld@users.noreply.github.com>
2022-09-16 12:31:13 +0900

    feat: Update naming for get current device info endpoint

--- src/routers/device.py
+++ src/routers/device.py
@@ -10,10 +10,10 @@ from src.db.db import get_db
 router = APIRouter()
 
 
-@router.get("/device-info/{device_id}", response_model=device_schema.Device)
-async def read_device_info(device_id: int, db: AsyncSession = Depends(get_db)) -> dict:
+@router.get("/device-data/{device_id}/live", response_model=device_schema.Device)
+async def read_device_data(device_id: int, db: AsyncSession = Depends(get_db)) -> dict:
     # TODO Need to authenticate before fetching the current data
-    result = await device_crud.get_current_device_info(db, device_id)
+    result = await device_crud.get_latest_device_data(db, device_id)
     return {
         "temperature_celsius": result[0],
         "humidity": result[1],
