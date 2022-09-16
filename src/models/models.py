0f3ce1b36c65e9c67061bbfdcef7ac8ddd4247fc
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-09-16 05:05:44 +0000

    feat: Add Mixin for create_at and updated_at

--- src/models/models.py
+++ src/models/models.py
@@ -1,10 +1,21 @@
+from datetime import datetime
+
 from sqlalchemy import Boolean, Column, DateTime, Float, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
 from src.db.db import Base
 
 
-class User(Base):
+class CreatedUpdatedDefaultTimeStampMixin:
+    created_at = Column(DateTime, default=datetime.now(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.now(), nullable=False)
+
+
+class CreatedNoDefaultTimeStampMixin:
+    created_at = Column(DateTime, nullable=False)
+
+
+class User(Base, CreatedUpdatedDefaultTimeStampMixin):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
@@ -12,21 +23,17 @@ class User(Base):
     last_name = Column(String(50))
     email = Column(String(100), unique=True, index=True)
     phone_number = Column(String(20))
-    created_at = Column(DateTime)
-    updated_at = Column(DateTime)
 
     devices = relationship("Device", back_populates="user")
 
 
-class Device(Base):
+class Device(Base, CreatedUpdatedDefaultTimeStampMixin):
     __tablename__ = "devices"
 
     id = Column(Integer, primary_key=True, index=True)
     latitude = Column(Float)
     longitude = Column(Float)
     device_name = Column(String(100))
-    created_at = Column(DateTime)
-    updated_at = Column(DateTime)
     user_id = Column(Integer, ForeignKey("users.id"))
 
     user = relationship("User", back_populates="devices")
@@ -39,68 +46,62 @@ class Device(Base):
     notification_data = relationship("Notification", back_populates="device")
 
 
-class Temperature(Base):
+class Temperature(Base, CreatedNoDefaultTimeStampMixin):
     __tablename__ = "temperature"
 
     id = Column(Integer, primary_key=True, index=True)
     temperature = Column(Float)
-    created_at = Column(DateTime)
     device_id = Column(Integer, ForeignKey("devices.id"))
 
     device = relationship("Device", back_populates="temperature_data")
 
 
-class Humidity(Base):
+class Humidity(Base, CreatedNoDefaultTimeStampMixin):
     __tablename__ = "humidity"
 
     id = Column(Integer, primary_key=True, index=True)
     humidity = Column(Float)
-    created_at = Column(DateTime)
     device_id = Column(Integer, ForeignKey("devices.id"))
 
     device = relationship("Device", back_populates="humidity_data")
 
 
-class Motion(Base):
+class Motion(Base, CreatedNoDefaultTimeStampMixin):
     __tablename__ = "motion"
 
     id = Column(Integer, primary_key=True, index=True)
     motion = Column(Boolean)
-    created_at = Column(DateTime)
     device_id = Column(Integer, ForeignKey("devices.id"))
 
     device = relationship("Device", back_populates="motion_data")
 
 
-class Notification(Base):
+class Notification(Base, CreatedNoDefaultTimeStampMixin):
     __tablename__ = "notifications"
 
     id = Column(Integer, primary_key=True, index=True)
     content = Column(String(300))
     is_read = Column(Boolean)
-    created_at = Column(DateTime)
     device_id = Column(Integer, ForeignKey("devices.id"))
 
     device = relationship("Device", back_populates="notification_data")
 
 
-class Button(Base):
+class Button(Base, CreatedNoDefaultTimeStampMixin):
     __tablename__ = "button"
 
     id = Column(Integer, primary_key=True, index=True)
     device_listening = Column(Boolean)
-    created_at = Column(DateTime)
     device_id = Column(Integer, ForeignKey("devices.id"))
 
     device = relationship("Device", back_populates="button_data")
 
 
-class Alarm(Base):
+class Alarm(Base, CreatedNoDefaultTimeStampMixin):
     __tablename__ = "alarm"
 
     id = Column(Integer, primary_key=True, index=True)
     is_alarm = Column(Boolean)
-    created_at = Column(DateTime)
     device_id = Column(Integer, ForeignKey("devices.id"))
 
     device = relationship("Device", back_populates="alarm_data")
