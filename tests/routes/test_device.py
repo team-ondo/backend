98588e6f6c120b3158ed4e2e60c6c5dd7bc790de
jonlld <87318928+jonlld@users.noreply.github.com>
2022-09-16 12:28:37 +0900

    chore: Add test notes for read_device_data

---
+++ tests/routes/test_device.py
@@ -0,0 +1,13 @@
+# from fastapi.testclient import TestClient
+
+# from src.routers.device import router
+
+# client = TestClient(router)
+
+
+# def test_read_device_data_existing_device_id() -> None:
+#     response = client.get("/device-data/1/live")
+#     assert response.status_code == 200
+
+
+# def test_read_device_data_non_existing_device_id() -> None:
