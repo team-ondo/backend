e97a3ac5aad4dc82b4a03b508aa0c6325fc42782
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-09-18 15:49:28 +0000

    feat: Include auth router

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 
-from src.routers import device, weather
+from src.routers import auth, device, weather
 
 app = FastAPI()
 app.include_router(device.router)
 app.include_router(weather.router)
+app.include_router(auth.router)
 
 
 @app.get("/")
