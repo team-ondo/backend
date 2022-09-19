fbd5f194707eac9f7feef6b269cccafcc4fd2e56
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-09-19 07:33:14 +0000

    chore: Add CORS configuration

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routers import device, weather
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(device.router)
 app.include_router(weather.router)
 
