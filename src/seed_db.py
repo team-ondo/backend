eacf97d3d3da7cbc445bd73a370304c6ae42da3f
jonlld <87318928+jonlld@users.noreply.github.com>
2022-09-20 15:32:04 +0900

    chore: Update seed file to include more data for temp and humidity

--- src/seed_db.py
+++ src/seed_db.py
@@ -1,4 +1,6 @@
+import datetime as date
 import os
+import random
 from datetime import datetime
 
 from dotenv import load_dotenv
@@ -15,19 +17,68 @@ engine = create_engine(SYNC_DATABASE_URL, echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+uuid = "a7382f5c33264cf8b717549affe1c2eb"
+
 seed_time = datetime.now()
 
-uuid = "a7382f5c33264cf8b717549affe1c2eb"
+
+def get_date():
+    # slightly randomizes day
+    return datetime.now() - date.timedelta(random.randint(1, 7))
+
 
 registered1 = Register(id=uuid, registered=True, created_at=seed_time, updated_at=seed_time)
 user1 = User(first_name="Akemi", last_name="Kimura", email="test@test.com", phone_number="111-2222-3333", created_at=seed_time, updated_at=seed_time)
 device1 = Device(id=uuid, latitude=35.6560, longitude=139.7247, device_name="Roppongi_Device", created_at=seed_time, updated_at=seed_time, user_id=1)
-temp1 = Temperature(temperature=28.7, created_at=seed_time, device_id=uuid)
-humidity1 = Humidity(humidity=54, created_at=seed_time, device_id=uuid)
+
+temp1 = Temperature(temperature=28.7, created_at=get_date(), device_id=uuid)
+humidity1 = Humidity(humidity=54, created_at=get_date(), device_id=uuid)
+temp2 = Temperature(temperature=28.7, created_at=get_date(), device_id=uuid)
+humidity2 = Humidity(humidity=54, created_at=get_date(), device_id=uuid)
+temp3 = Temperature(temperature=28.7, created_at=get_date(), device_id=uuid)
+humidity3 = Humidity(humidity=54, created_at=get_date(), device_id=uuid)
+temp4 = Temperature(temperature=28.7, created_at=get_date(), device_id=uuid)
+humidity4 = Humidity(humidity=54, created_at=get_date(), device_id=uuid)
+temp5 = Temperature(temperature=28.7, created_at=get_date(), device_id=uuid)
+humidity5 = Humidity(humidity=54, created_at=get_date(), device_id=uuid)
+temp6 = Temperature(temperature=28.7, created_at=get_date(), device_id=uuid)
+humidity6 = Humidity(humidity=54, created_at=get_date(), device_id=uuid)
+temp7 = Temperature(temperature=28.7, created_at=get_date(), device_id=uuid)
+humidity7 = Humidity(humidity=54, created_at=get_date(), device_id=uuid)
+temp8 = Temperature(temperature=28.7, created_at=get_date(), device_id=uuid)
+humidity8 = Humidity(humidity=54, created_at=get_date(), device_id=uuid)
+
 motion1 = Motion(motion=True, created_at=seed_time, device_id=uuid)
 notification1 = Notification(content="Please call", is_read=False, created_at=seed_time, device_id=uuid)
 button1 = Button(device_listening=True, created_at=seed_time, device_id=uuid)
 alarm1 = Alarm(is_alarm=False, created_at=seed_time, device_id=uuid)
 
-session.add_all([registered1, user1, device1, temp1, humidity1, motion1, notification1, button1, alarm1])
+session.add_all(
+    [
+        registered1,
+        user1,
+        device1,
+        temp1,
+        humidity1,
+        temp2,
+        humidity2,
+        temp3,
+        humidity3,
+        temp4,
+        humidity4,
+        temp6,
+        humidity6,
+        temp7,
+        humidity7,
+        temp8,
+        humidity8,
+        motion1,
+        notification1,
+        button1,
+        alarm1,
+    ]
+)
+
+# session.add_all(test_temp_humidity)
+
 session.commit()
