0bb368b8a02b87b601258f8b65a5f9a24babf561
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-09-20 03:56:17 +0000

    fix: When seeding user, set hash password too

--- src/seed_db.py
+++ src/seed_db.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from src.auth.utils import create_hash_password
 from src.models.models import Alarm, Button, Device, Humidity, Motion, Notification, Register, Temperature, User
 
 load_dotenv()
@@ -20,7 +21,15 @@ seed_time = datetime.now()
 uuid = "a7382f5c33264cf8b717549affe1c2eb"
 
 registered1 = Register(id=uuid, registered=True, created_at=seed_time, updated_at=seed_time)
-user1 = User(first_name="Akemi", last_name="Kimura", email="test@test.com", phone_number="111-2222-3333", created_at=seed_time, updated_at=seed_time)
+user1 = User(
+    first_name="Akemi",
+    last_name="Kimura",
+    email="test@test.com",
+    phone_number="111-2222-3333",
+    password=create_hash_password("secret"),
+    created_at=seed_time,
+    updated_at=seed_time,
+)
 device1 = Device(id=uuid, latitude=35.6560, longitude=139.7247, device_name="Roppongi_Device", created_at=seed_time, updated_at=seed_time, user_id=1)
 temp1 = Temperature(temperature=28.7, created_at=seed_time, device_id=uuid)
 humidity1 = Humidity(humidity=54, created_at=seed_time, device_id=uuid)
