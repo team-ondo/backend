23881634d97aa54f55f98d546117406d41e0608b
keigrey <keixgrey@gmail.com>
2022-09-21 03:20:54 +0000

    feat: Send test message

--- src/routers/notification.py
+++ src/routers/notification.py
@@ -11,13 +11,19 @@ from src.db.db import get_db
 load_dotenv()
 router = APIRouter()
 
-OPEN_WEATHER_APPID = os.getenv("OPEN_WEATHER_APPID")
 TWILIO_SID = os.getenv("TWILIO_SID")
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 TWILIO_VIRTUAL_NUMBER = os.getenv("TWILIO_VIRTUAL_NUMBER")
 TWILIO_VERIFIED_NUMBER = os.getenv("TWILIO_VERIFIED_NUMBER")
 
 
-@router.get("/device/{device_id}/alarm/on")
+@router.post("/device/{device_id}/alarm/on")
 async def device_alarm_on(device_id: str = Path(regex=RE_UUID)):
-    pass
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    notification_text = "TEST MESSAGE FROM ONDO BACKEND"
+
+    message = client.messages.create(body=notification_text, from_=TWILIO_VIRTUAL_NUMBER, to=TWILIO_VERIFIED_NUMBER)
+
+    # print if successfully sent
+    print(message.sid)
