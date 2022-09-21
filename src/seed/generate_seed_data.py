1991c0d69ba4768db4b7aa66d8deb6a78686fe5b
jonlld <87318928+jonlld@users.noreply.github.com>
2022-09-21 11:08:07 +0000

    chore: Add additional data points for last 24 hours to populate graph

--- src/seed/generate_seed_data.py
+++ src/seed/generate_seed_data.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import Dict, List
 
 from faker import Faker
@@ -7,6 +7,7 @@ fake = Faker()
 
 UUID = "a7382f5c33264cf8b717549affe1c2eb"
 NUM_OF_DATA_POINTS = 10000
+NUM_OF_DATA_POINTS_FOR_DAY = 1000
 
 
 def generate_historic_temp_data() -> Dict[str, List]:
@@ -20,6 +21,17 @@ def generate_historic_temp_data() -> Dict[str, List]:
         temp = fake.random_int(min=13, max=36)
         humid = fake.random_int(min=50, max=80)
         date = fake.date_time_between(start_date=datetime(2022, 7, 21), end_date=datetime(2022, 9, 21)).strftime("%Y-%m-%d %H:%M:%S")
+
+        temp_obj = {"temperature": temp, "created_at": date, "device_id": UUID}
+        humid_obj = {"humidity": humid, "created_at": date, "device_id": UUID}
+
+        result["temp"].append(temp_obj)
+        result["humid"].append(humid_obj)
+
+    for _ in range(NUM_OF_DATA_POINTS_FOR_DAY):
+        temp = fake.random_int(min=13, max=36)
+        humid = fake.random_int(min=50, max=80)
+        date = fake.date_time_between(start_date=datetime.now() - timedelta(days=1), end_date=datetime.now()).strftime("%Y-%m-%d %H:%M:%S")
 
         temp_obj = {"temperature": temp, "created_at": date, "device_id": UUID}
         humid_obj = {"humidity": humid, "created_at": date, "device_id": UUID}
