c442575720d8f9048afed044b6dcc1f4410b36c6
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-09-21 09:54:53 +0000

    feat: Add GET /settings/device endpoint

--- src/routers/settings.py
+++ src/routers/settings.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -13,3 +15,8 @@ router = APIRouter()
 @router.get("/settings/user", response_model=settings_schema.UserSettings)
 async def read_user_settings(current_user: auth_schema.SystemUser = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
     return await settings_cruds.find_user_settings_by_user_id(db, current_user.user_id)
+
+
+@router.get("/settings/device", response_model=List[settings_schema.DeviceSettings])
+async def read_device_settings(current_user: auth_schema.SystemUser = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
+    return await settings_cruds.find_device_settings_by_user_id(db, current_user.user_id)
