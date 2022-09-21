02d297b8801f5e85db4b36daed388ccd98b8a651
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-09-21 03:22:13 +0000

    feat: Add GET /settings/user endpoint

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from src.routers import auth, device, weather
+from src.routers import auth, device, settings, weather
 
 app = FastAPI()
 app.add_middleware(
@@ -14,6 +14,7 @@ app.add_middleware(
 app.include_router(device.router)
 app.include_router(weather.router)
 app.include_router(auth.router)
+app.include_router(settings.router)
 
 
 @app.get("/")
