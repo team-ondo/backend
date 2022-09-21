6c107c0a63b73b7ea0a9d4c88bde61e492acd835
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-09-21 07:14:27 +0000

    fix: Change the humidity type to float

--- src/schemas/device.py
+++ src/schemas/device.py
@@ -7,10 +7,7 @@ class Device(BaseModel):
     temperature_celsius: float | None = Field(None, example="25.1", description="Temperature (Celsius)")
     # TODO Add later
     # temperature_fahrenheit: float | None = Field(None, example="77.18", description="Temperature (Fahrenheit)")
-    humidity: int | None = Field(None, example="87", description="Humidity (Percentage)")
-
-    class Config:
-        orm_mode = True
+    humidity: float | None = Field(None, example="87", description="Humidity (Percentage)")
 
 
 class DeviceHistorical(BaseModel):
