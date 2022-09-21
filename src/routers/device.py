f04b6753b8ff21d32d4524d07d846a4ff23fbba8
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-09-21 07:15:19 +0000

    fix: Remove return type hint. Return Device instance

--- src/routers/device.py
+++ src/routers/device.py
@@ -12,14 +12,11 @@ router = APIRouter()
 
 
 @router.get("/device-data/{device_id}/live", response_model=device_schema.Device)
-async def read_device_data(device_id: str = Path(regex=RE_UUID), db: AsyncSession = Depends(get_db)) -> dict:
+async def read_device_data(device_id: str = Path(regex=RE_UUID), db: AsyncSession = Depends(get_db)):
     # TODO Need to authenticate before fetching the current data
     # TODO Check device exists, and owned by user.
     result = await device_crud.get_latest_device_data(db, device_id)
-    return {
-        "temperature_celsius": result[0],
-        "humidity": result[1],
-    }
+    return device_schema.Device(temperature_celsius=result[0], humidity=result[1])
 
 
 @router.post("/device-data/{device_id}")
