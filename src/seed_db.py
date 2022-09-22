bd5a137f3bc0693a212fbbcf1a88b9c19f422bbc
jonlld <87318928+jonlld@users.noreply.github.com>
2022-09-22 08:15:20 +0000

    chore: Add generated alarm data to seed db

--- src/seed_db.py
+++ src/seed_db.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import sessionmaker
 
 from src.auth.utils import create_hash_password
 from src.models.models import Alarm, Button, Device, Humidity, Motion, Notification, Register, Temperature, User
-from src.seed.generate_seed_data import generate_historic_temp_data
+from src.seed.generate_seed_data import generate_historic_alarm_data, generate_historic_temp_data
 
 load_dotenv()
 SYNC_DATABASE_URL = os.getenv("SYNC_DATABASE_URL")
@@ -69,7 +69,11 @@ for row in generated_data_set.get("humid"):
 data_set.append(Motion(motion=True, created_at=seed_time, device_id=uuid))
 data_set.append(Notification(content="Please call", is_read=False, created_at=seed_time, device_id=uuid))
 data_set.append(Button(device_listening=True, created_at=seed_time, device_id=uuid))
-data_set.append(Alarm(is_alarm=False, created_at=seed_time, device_id=uuid))
+
+generated_data_set_alarm = generate_historic_alarm_data()
+
+for row in generated_data_set_alarm.get("alarm"):
+    data_set.append(Alarm(**row))
 
 session.add_all(data_set)
 
