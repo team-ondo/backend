2efc2eed0e0e8b6079b243fd75e4b6691509dbc1
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-09-23 08:08:39 +0000

    feat: Add UpdateUserSettings schema

--- src/schemas/settings.py
+++ src/schemas/settings.py
@@ -1,8 +1,8 @@
 from typing import Any, Dict, Optional
 
-from pydantic import UUID4, BaseModel, Field, root_validator
+from pydantic import UUID4, BaseModel, EmailStr, Field, root_validator
 
-from src.utils.common import fetch_longitude_latitude_from_zip_code, is_zip_code
+from src.utils.common import fetch_longitude_latitude_from_zip_code, is_phone_number, is_zip_code
 
 
 class UserSettings(BaseModel):
@@ -50,5 +50,31 @@ class UpdateDeviceSettings(BaseModel):
             longitude, latitude = fetch_longitude_latitude_from_zip_code(zip_code)
             cls.longitude = longitude
             cls.latitude = latitude
+        return values
 
+
+class UpdateUserSettings(BaseModel):
+    first_name: Optional[str] = Field(None, min_length=1, max_length=50, example="John", description="First name")
+    last_name: Optional[str] = Field(None, min_length=1, max_length=50, example="Doe", description="Last name")
+    email: Optional[EmailStr] = Field(None, example="example@example.com", description="Email address")
+    phone_number: Optional[str] = Field(
+        None, max_length=20, example="090-0000-0000", description="Phone number(Landline phone, Wireless phone numbers only)"
+    )
+    old_password: Optional[str] = Field(None, min_length=8, max_length=16, example="secretPassword!", description="Old password")
+    new_password: Optional[str] = Field(None, min_length=8, max_length=16, example="secretPassword!", description="New password")
+
+    @root_validator()
+    def validate_all(cls, values: Dict[str, Any]) -> Dict[str, Any]:
+        # Validate phone number
+        phone_number = values.get("phone_number")
+        if phone_number is not None and not is_phone_number(phone_number):
+            raise ValueError("Phone number format is not correct")
+
+        # Validate password
+        old_password = values.get("old_password")
+        new_password = values.get("new_password")
+        if old_password is not None and new_password is None:
+            raise ValueError("New password is missing")
+        if new_password is not None and old_password is None:
+            raise ValueError("Old password is missing")
         return values
