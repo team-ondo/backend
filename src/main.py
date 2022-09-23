aaf7cb1eb3b7d88aa7ab10d5442e1c526f16d8af
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-09-23 11:34:24 +0000

    chore: Use starlette middleware

--- src/main.py
+++ src/main.py
@@ -1,31 +1,33 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 
 from src.errors.errors import APIError
 from src.routers import auth, device, notification, settings, user, weather
 
-app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+]
 
-
-@app.exception_handler(APIError)
-async def api_error_handler(request, err: APIError):
-    return JSONResponse(status_code=err.status_code, content={"detail": err.detail}, headers=err.headers)
-
-
+app = FastAPI(middleware=middleware)
 app.include_router(device.router)
 app.include_router(weather.router)
 app.include_router(auth.router)
 app.include_router(settings.router)
 app.include_router(notification.router)
 app.include_router(user.router)
+
+
+@app.exception_handler(APIError)
+async def api_error_handler(request, err: APIError):
+    return JSONResponse(status_code=err.status_code, content={"detail": err.detail}, headers=err.headers)
 
 
 @app.get("/")
