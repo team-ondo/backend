288dd2885070b1b821505dae560347df56a73c6c
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-09-24 15:39:33 +0000

    feat: Add PUT /notifications/{notification_id} endpoint

--- src/routers/notification.py
+++ src/routers/notification.py
@@ -1,11 +1,17 @@
 import os
 
 from dotenv import load_dotenv
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Depends, Path
+from sqlalchemy.ext.asyncio import AsyncSession
 from twilio.rest import Client
 
+import src.cruds.notification as notification_crud
+import src.schemas.auth as auth_schema
 import src.schemas.notification as notification_schema
 from src.constants.common import RE_UUID
+from src.db.db import get_db
+from src.errors.errors import IncorrectUserUpdate, TokenExpiredException, TokenValidationFailException, UserNotFoundException, error_response
+from src.routers.auth import get_current_user
 
 load_dotenv()
 router = APIRouter()
@@ -28,3 +34,25 @@ async def device_alarm_on(notification_status: notification_schema.NotificationS
     print(message.sid)
 
     # TODO Handle failed messages
+
+
+@router.put(
+    "/notifications/{notification_id}",
+    responses=error_response(
+        [
+            UserNotFoundException,
+            TokenValidationFailException,
+            TokenExpiredException,
+            IncorrectUserUpdate,
+        ]
+    ),
+)
+async def update_notification(
+    current_user: auth_schema.SystemUser = Depends(get_current_user),
+    notification_id: int = Path(example=1, description="Notification id"),
+    db: AsyncSession = Depends(get_db),
+):
+    notification_belongs_to_user = await notification_crud.notification_belongs_to_user(db, notification_id, current_user.user_id)
+    if not notification_belongs_to_user:
+        raise IncorrectUserUpdate()
+    await notification_crud.update_notification(db, notification_id)
