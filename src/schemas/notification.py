f00f017df01177787a4b194332bcc07ac5943124
jonlld <87318928+jonlld@users.noreply.github.com>
2022-09-24 04:08:27 +0000

    feat: Add schema for device notifications data

--- src/schemas/notification.py
+++ src/schemas/notification.py
@@ -1,5 +1,14 @@
+from datetime import datetime
+
 from pydantic import BaseModel, Field
 
 
 class NotificationStatus(BaseModel):
     message: str = Field(example="It's too cold in the room! Please check on your loved one", description="Status message")
+
+
+class NotificationData(BaseModel):
+    content_type: str = Field(example="Alarm", description="Type of notification to be pushed.")
+    content: str = Field(example="Alarm was triggered", description="Content for the notification.")
+    is_read: bool = Field(example="True", description="Boolean value to show read / unread status.")
+    created_at: datetime = Field(example="2022-07-01 12:23:45", description="Created timestamp")
