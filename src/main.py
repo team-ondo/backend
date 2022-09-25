8447740c10a6dc0b27d8b1a0ef06f82f9b35b179
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-09-25 15:48:52 +0000

    feat: Use Socketio with fastapi

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from fastapi_socketio import SocketManager
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 
 from src.errors.errors import APIError
 from src.routers import auth, device, notification, settings, user, weather
+from src.sockets.hardware_namespace import HardwareNameSpace
 
 middleware = [
     Middleware(
@@ -30,6 +32,15 @@ async def api_error_handler(request, err: APIError):
     return JSONResponse(status_code=err.status_code, content={"detail": err.detail}, headers=err.headers)
 
 
+socket_manager = SocketManager(app)
+socket_manager._sio.register_namespace(HardwareNameSpace("/hardware"))
+
+
 @app.get("/")
 async def root() -> dict:
     return {"message": "Hello World"}
+
+
+from src.routers import alarm  # noqa: E402
+
+app.include_router(alarm.router)
